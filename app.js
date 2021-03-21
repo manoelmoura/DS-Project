103bc296c36e8eeb67699cbc1a614d1e92656f1b
Leonardo Fernandes Padre <62120616+leeooozin@users.noreply.github.com>
2021-03-21 12:47:44 -0300

    Esqueleto projeto
    Esqueleto principal do projeto

---
+++ app.js
@@ -0,0 +1,61 @@
+// Carregando Módulos   
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+    const path = require('path')
+    const mongoose = require('mongoose')
+    const session = require('session')
+    const flash = require('connect-flash')
+
+// Config
+    // Session
+        app.use(session({
+            secret: 'guiaunb',
+            resave: true,
+            saveUninitialized: true
+        }))
+
+        app.use(passport.initialize())
+        app.use(passport.session())
+
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            res.locals.error = req.flash('error')
+            res.locals.user = req.user || null;
+            next()
+        })
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Mongoose
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/guiaunb').then(() => {
+            console.log('Conectado ao mongo')
+        }).catch((err) => {
+            console.log('Erro ao se conectar: ' + err)
+        })
+    // Public
+        app.use(express.static(path.join(__dirname, 'public')))
+
+        app.use((req, res, next) => {
+            console.log('MIDDLEWARE')
+            next()
+        })
+
+
+// Routes
+    //
+
+
+    // Outros
+    const PORT = 8081
+    app.listen(PORT, () => {
+        console.log('Servidor Rodando!')
+    })
